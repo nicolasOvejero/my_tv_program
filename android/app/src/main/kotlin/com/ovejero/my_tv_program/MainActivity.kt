3bb32b51919052ba7449739f4b9363ca56af0af0
Ovejero Nicolas <nicolas.ovejero.epitech@gmail.com>
2021-02-02 21:15:46 +0100

    feat: Initial commit

---
+++ android/app/src/main/kotlin/com/ovejero/my_tv_program/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ovejero.my_tv_program
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:xml/xml.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mon programme TV',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    final bookshelfXml = '''<?xml version="1.0"?>
+    <bookshelf>
+      <book>
+        <title lang="english">Growing a Language</title>
+        <price>29.99</price>
+      </book>
+      <book>
+        <title lang="english">Learning XML</title>
+        <price>39.95</price>
+      </book>
+      <price>132.00</price>
+    </bookshelf>''';
+    final document = XmlDocument.parse(bookshelfXml);
+    print(document);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          //
+          //
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
