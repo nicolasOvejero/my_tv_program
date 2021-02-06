d930af13c066396352cfce7a2e69532121c15a04
Nikos <nicolas.ovejero@neo9.fr>
2021-02-06 21:29:49 +0100

    feat: Add custom cache on request body for 2 hours

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -1,11 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 class HttpService {
   static Future<String> getXmlValuesFormServer() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/programmes.txt');
+
+    if (await file.exists()) {
+      DateTime lastModifies = await file.lastModified();
+      if (DateTime.now().subtract(Duration(hours: 2)).isBefore(lastModifies)) {
+        return await file.readAsString();
+      } else {
+        file.writeAsString('');
+      }
+    }
+
     final Map<String, String> headers = {"Accept": "text/html,application/xml"};
     final response = await http.get('https://xmltv.ch/xmltv/xmltv-tnt.xml', headers: headers);
 
     if (response.statusCode == 200) {
+      file.writeAsString(response.body);
       return response.body;
     }
 
